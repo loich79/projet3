a36941af14e3bb94ad10b58a3384a85a5b37be6a
loich79 <loic.hay79@gmail.com>
2017-05-19 04:01:38 +0200

    added modifications on public folder's file

--- Public/index.php
+++ Public/index.php
@@ -4,7 +4,8 @@
 define('ROOT', dirname(__DIR__));
 // définit un mot clé MAX_COMMENT_LEVEL pour le niveau maximum de réponses autorisé
 define('MAX_COMMENT_LEVEL', 3);
-
+// définit un mot clé NB_POSTS_PER_PAGE pour le nombre d'article par page
+define('NB_POSTS_PER_PAGE', 5);
 require ROOT.'/App/App.php';
 App::load();
 // definition de la page à appeler
